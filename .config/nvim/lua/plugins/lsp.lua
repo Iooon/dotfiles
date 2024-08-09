650a6be61c3096b1f1a651066491e5db1579c8fc
crispaudio <jmueller@crispaudio.de>
2024-08-09 13:21:58 +0200

    Remove auto format on save

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -88,19 +88,6 @@ return {
         local nvim_lsp = require("lspconfig")
         local mason_lspconfig = require("mason-lspconfig")
 
-        local on_attach = function(client, bufnr)
-            -- format on save
-            if client.server_capabilities.documentFormattingProvider then
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    group = vim.api.nvim_create_augroup("Format", { clear = true }),
-                    buffer = bufnr,
-                    callback = function()
-                        vim.lsp.buf.format()
-                    end,
-                })
-            end
-        end
-
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
         mason_lspconfig.setup_handlers({
@@ -111,25 +98,21 @@ return {
             end,
             ["lua_ls"] = function()
                 nvim_lsp["lua_ls"].setup({
-                    on_attach = on_attach,
                     capabilities = capabilities,
                 })
             end,
             ["clangd"] = function()
                 nvim_lsp["clangd"].setup({
-                    on_attach = on_attach,
                     capabilities = capabilities,
                 })
             end,
             ["rust_analyzer"] = function()
                 nvim_lsp["rust_analyzer"].setup({
-                    on_attach = on_attach,
                     capabilities = capabilities,
                 })
             end,
             ["pyright"] = function()
                 nvim_lsp["pyright"].setup({
-                    on_attach = on_attach,
                     capabilities = capabilities,
                 })
             end,
